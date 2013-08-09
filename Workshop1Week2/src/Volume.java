8b3fd49c66f3eae1c30aadaa4ecfa6ef8b931495
lberezy <lberezy@56d1951d-bf66-49da-99e5-76cc4cbb8347>
2013-08-09 02:15:04 +0000

    Reverted changes.

--- Workshop1Week2/src/Volume.java
+++ Workshop1Week2/src/Volume.java
@@ -7,15 +7,21 @@ public class Volume {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double r, volume;
+		double radius, volume;
 		if(args.length == 1){
-			r = Double.parseDouble(args[0]);
-			volume = 4/3.0*java.lang.Math.PI*r*r;
-			System.out.println("Radius: " + r);
+			radius = Double.parseDouble(args[0]);
+			volume = calculateVolume(radius);
+			System.out.println("Radius: " + radius);
 			System.out.println("Volume: " + volume);
+			//
+			System.out.println("Stuff");
 		} else {
 			System.out.println("Submit 'radius' as a single command line argument.");
 		}
+	}
+	
+	public static double calculateVolume(double r){
+		return 4/3.0*java.lang.Math.PI*r*r;
 	}
 	
 }
