41bad510b3cf00e4f15783667279801a52eb788a
lberezy <lberezy@56d1951d-bf66-49da-99e5-76cc4cbb8347>
2013-08-09 01:43:05 +0000

    Added Volume of sphere calculation class.

---
+++ oosd/Workshop1Week2/src/Volume.java
@@ -0,0 +1,25 @@
+
+
+public class Volume {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius, volume;
+		if(args.length == 1){
+			radius = Double.parseDouble(args[0]);
+			volume = calculateVolume(radius);
+			System.out.println("Radius: " + radius);
+			System.out.println("Volume: " + volume);
+		} else {
+			System.out.println("Submit 'radius' as a single command line argument.");
+		}
+	}
+	
+	public static double calculateVolume(double r){
+		return 4/3.0*java.lang.Math.PI*r*r;
+	}
+	
+}
