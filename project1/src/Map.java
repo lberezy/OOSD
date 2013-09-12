2629ce47cb2de54aa7ec2c7dc4b126a2bf111e33
lberezy <lberezy@56d1951d-bf66-49da-99e5-76cc4cbb8347>
2013-09-12 14:17:41 +0000

    Working for submission. Added cross platform library support. Player can be pushed off screen.

--- project1/src/Map.java
+++ project1/src/Map.java
@@ -7,13 +7,12 @@ public class Map extends TiledMap{
 	public Map(String mapFile, String assetsPath) throws SlickException {
 		super(mapFile, assetsPath);
 		
-		// build an array of blocked tiles, faster than always checking the map
+		// build an array of blocked tiles, faster than always checking the map tiles
 		blockTiles = new Boolean[this.getWidth()][this.getHeight()];
-		for (int x = 0; x < getWidth() - 1; x++) {
-			for (int y = 0; y < getHeight() - 1; y++) {
+		for (int x = 0; x < getWidth(); x++) {
+			for (int y = 0; y < getHeight(); y++) {
 		    	int tileID = this.getTileId(x, y, 0);	// get tileID at point, layer 0 of tilemap
-				System.out.println(x + " " + y + " " + tileID );
-		    	System.out.println(blockTiles[x][y] = this.getTileProperty(tileID, "block", "0").equals("1"));
+		    	blockTiles[x][y] = this.getTileProperty(tileID, "block", "0").equals("1");
 			}
 		}
 		
@@ -23,26 +22,31 @@ public class Map extends TiledMap{
     	/** checks if a game coordinate is a valid map tile location
     	 * 
     	 */
-    	double x = point.x;
-    	double y = point.y;
+    	int x = (int)point.x;
+    	int y = (int)point.y;
     	return isValidPoint(x, y);
     }
     
-    public Boolean checkBlock(int x, int y) {
-    	return blockTiles[x][y];
-    }
-    
-    
-    public Boolean isValidPoint(double x, double y) {
+    public Boolean isValidPoint(int x, int y) {
     	/** checks if a game coordinate is a valid map tile location
     	 * 
     	 */
     	if (x < 0 || x/this.getWidth() > this.getWidth()) {
     		return false;
     	}
-    	if(y < 0 || x/this.getHeight() > this.getHeight()) {
+    	if(y < 0 || y/this.getHeight() > this.getHeight()) {
     		return false;
     	}
     	return true;
     }
+    
+    public Boolean checkBlock(int x, int y) {
+    	/** Checks if a map tile is a blocking tile
+    	 * 
+    	 */
+    	return blockTiles[x][y];
+    }
+    
+    
+
 }
